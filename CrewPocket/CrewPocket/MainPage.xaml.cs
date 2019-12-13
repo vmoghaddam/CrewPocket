84edd2c8cc2dc3fb72c71c64c2b7f8110efcbbd0
Vahid <v.moghaddam59@gmail.com>
2019-12-13 16:33:53 +0330

    hi

--- CrewPocket/CrewPocket/MainPage.xaml.cs
+++ CrewPocket/CrewPocket/MainPage.xaml.cs
@@ -11,6 +11,7 @@ namespace CrewPocket
 {
     // Learn more about making custom code visible in the Xamarin.Forms previewer
     // by visiting https://aka.ms/xamarinforms-previewer
+    //dool
     [DesignTimeVisible(false)]
     public partial class MainPage : ContentPage
     {
